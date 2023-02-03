5ae2933847fc6ef5f0e6a2226f60a0f0b669b4ec
JohanWoel <johanwoellala@gmail.com>
2023-02-03 20:27:56 +0300

    commit

---
+++ src/test/java/com/example/gestionEncheres/GestionEncheresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionEncheres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEncheresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
